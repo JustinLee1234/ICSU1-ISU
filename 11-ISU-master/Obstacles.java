9a9763fd2801bb918ad276bdd9ee7cf19905fa45
335399705 <59704584+335399705@users.noreply.github.com>
2020-01-14 13:48:57 -0500

    Add files via upload

---
+++ 11-ISU-master/Obstacles.java
@@ -0,0 +1,22 @@
+/* Obstacles
+ * January 9, 2020
+ * Justin Lee & Marian Wong
+ */
+import java.awt.Graphics;
+import java.awt.Color;
+
+public class Obstacles extends Entity {
+
+  private String direction;
+  
+  public Obstacles (int length, int width, String direction){  
+    this.direction = direction;
+
+    
+  }
+  
+ 
+}
+  
+
+   
