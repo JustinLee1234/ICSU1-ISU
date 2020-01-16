52b908b0954391f48c6f059779cb735443fceabd
335399705 <59704584+335399705@users.noreply.github.com>
2020-01-16 13:42:30 -0500

    Update Main.java

--- 11-ISU-master/Main.java
+++ 11-ISU-master/Main.java
@@ -1,11 +1,11 @@
-import java.util.Scanner;
+import java.util.Scanner; //Import functions
 import java.io.File;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-public class Main{
+public class Main{ // Main Class
   
-  public static void main(String[] args) throws Exception{ 
-    int lineCount = 0;    
+  public static void main(String[] args) throws Exception{  
+    int lineCount = 0;    // Setting variables
     Scanner mapReader;
     int height = 15;
     int width = 15;
@@ -18,9 +18,9 @@ public class Main{
     TileMap t = new TileMap(p);
     p.loadImages();
     
-    while (inPlay){
+    while (inPlay){ 
       
-      for (int map = 0;  map < numMaps; map++){
+      for (int map = 0;  map < numMaps; map++){ //Goes through the different maps/ level layouts
         mapReader = new Scanner(new File("assets/maps/map" + (map+1) + ".txt"));
         for(int i = 0; i < height; i++) {
           line = mapReader.next();
@@ -31,7 +31,7 @@ public class Main{
         mapReader.close();
       }
       //if (p.colliding()){
-        player.move();
+        player.move(); //Moves player
       
     }
   }
