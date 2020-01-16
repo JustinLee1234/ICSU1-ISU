1a930d9a0625075ea4acab18c85a08e76044686f
335399705 <59704584+335399705@users.noreply.github.com>
2020-01-16 14:07:58 -0500

    Update Obstacles.java

--- 11-ISU-master/Obstacles.java
+++ 11-ISU-master/Obstacles.java
@@ -2,7 +2,7 @@
  * January 9, 2020
  * Justin Lee & Marian Wong
  */
-import java.awt.Graphics;
+import java.awt.Graphics; //import classes
 import java.awt.Color;
 
 public class Obstacles extends Entity {
