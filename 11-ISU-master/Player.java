4fb9fc16224f39842cd73da2f36e0dde87b798d4
335399705 <59704584+335399705@users.noreply.github.com>
2020-01-16 13:22:34 -0500

    Update Player.java

--- 11-ISU-master/Player.java
+++ 11-ISU-master/Player.java
@@ -1,4 +1,4 @@
-import java.awt.Graphics;
+import java.awt.Graphics; //Import functions
 import java.awt.Image;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -8,7 +8,7 @@ import java.awt.Rectangle;
 
 public class Player extends Entity implements KeyListener {
   
-  private int lives;
+  private int lives; //Create private and public variables
   private boolean dead;
   private BufferedImage sprite;
   public boolean movingRight;
@@ -19,11 +19,11 @@ public class Player extends Entity implements KeyListener {
   
   
   public Player(){
-    this.lives = lives;
+    this.lives = lives; 
   }  
   
   public void setLives(int lives){
-    this.lives = lives;
+    this.lives = lives; 
   }
   public int getLives(){
     return lives;
@@ -31,24 +31,24 @@ public class Player extends Entity implements KeyListener {
   
   
   public void move(){
-    boolean moving = false;
-    if (movingRight ||  movingLeft || movingUp || movingDown){
+    boolean moving = false; //The player does not move unless true
+    if (movingRight ||  movingLeft || movingUp || movingDown){ // If a key is pressed (it is true), add numbers to count
       count++;
-      if ((count%8) == 0){
-        moving = true;
+      if ((count%8) == 0){ // Allows movement of 8 pixels per tick speed
+        moving = true; //Movement occurs
       }
     }
     if (moving){
       if (movingRight){
-        setX(getX() + 1);
+        setX(getX() + 1); // If right key is pressed, add one to the x coordinates of player
       }
-      if (movingLeft){
+      if (movingLeft){// If left key is pressed, deduct one to the x coordinates of player
         setX(getX() - 1);
       }
-      if(movingUp){
+      if(movingUp){// If up key is pressed, deduct one to the y coordinates of player
         setY(getY() - 1);
       }
-      if(movingDown){
+      if(movingDown){// If down key is pressed, add one to the y coordinates of player
         setY(getY() + 1 );
       }
   }
@@ -61,10 +61,10 @@ public class Player extends Entity implements KeyListener {
     
   }
   
-  public void keyPressed(KeyEvent e) {
+  public void keyPressed(KeyEvent e) { //Method tracking the pressed keys
     int key = e.getKeyChar();
     
-    if (key == 'a') {
+    if (key == 'a') { // Depending on the character pressed, the direction of the player changes
       movingLeft = true;
     }
     
@@ -81,9 +81,9 @@ public class Player extends Entity implements KeyListener {
     }
   }
   
-  public void keyReleased(KeyEvent e) {
+  public void keyReleased(KeyEvent e) {// Method tracking released keys
     
-    int key = e.getKeyChar();
+    int key = e.getKeyChar(); // Depending on the character key released, the movement in that direction stops
     
     if (key == 'a') {
       movingLeft = false;
