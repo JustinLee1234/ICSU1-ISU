b0a4e5978fd8a9fbf3b9ac6168045a9a96f74ccc
335399705 <59704584+335399705@users.noreply.github.com>
2020-01-17 22:41:23 -0500

    Add files via upload

--- 11-ISU-master/TilePanel.java
+++ 11-ISU-master/TilePanel.java
@@ -15,6 +15,12 @@ import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.File;
 
+
+ import javax.swing.*;   //contains all "J" Components
+ import java.awt.*; 
+ import java.awt.event.*; 
+
+
 public class TilePanel extends JPanel{
   private char map[][];
   private final int TILE_SIZE = 50;
@@ -29,11 +35,18 @@ public class TilePanel extends JPanel{
   private int coinY;
   private int obstacleX;
   private int obstacleY;
+  private boolean home;
+  private boolean instructions;
+  private boolean levels;
   private static BufferedImage playerSprite;
   private static BufferedImage heartCanister;
   private static BufferedImage emptyCanister;
   private static BufferedImage obstacle;
   private static BufferedImage coin;
+  private static BufferedImage homeBg;
+  private static BufferedImage buttonTxt;
+  private static BufferedImage instructionButtonTxt;
+  private static BufferedImage titleTxt;
   public boolean collidable;
 
   
@@ -118,6 +131,10 @@ public class TilePanel extends JPanel{
       emptyCanister = ImageIO.read(new File(".\\assets\\sprites\\heartLost.png"));
       obstacle = ImageIO.read(new File (".\\assets\\sprites\\obstacle.png"));
       coin = ImageIO.read(new File(".\\assets\\sprites\\coin.png"));
+      homeBg = ImageIO.read(new File(".\\assets\\sprites\\homeScreen.png"));
+      buttonTxt = ImageIO.read(new File(".\\assets\\sprites\\buttonTxt.png"));
+      instructionButtonTxt = ImageIO.read(new File(".\\assets\\sprites\\instructionButtonTxt.png"));
+      titleTxt = ImageIO.read(new File(".\\assets\\sprites\\titleTxt.png"));
     }
     catch(Exception e){
       System.out.println("Image file not found");
@@ -125,52 +142,101 @@ public class TilePanel extends JPanel{
   }
   
   public void paintComponent(Graphics g){
+    super.paintComponent(g);
+    Graphics2D g2d = (Graphics2D) g;
+    JFrame Frame = new JFrame();
     
     super.repaint();
     setDoubleBuffered(true);
     
-    for(int i = 0; i<15;i++)  { 
-      for(int j = 0; j<15;j++)  {
-        if (map[i][j] == 'x')
-          g.setColor(Color.DARK_GRAY);
-        else if (map[i][j] == 'z')
-          g.setColor(Color.PINK);
-        else if (map[i][j] == 'b'){
-          g.setColor(Color.CYAN);
-          obstacleX = j* TILE_SIZE;
-          obstacleY = i * TILE_SIZE;
-        }
-        else if (map[i][j] == '0')
-          g.setColor(Color.CYAN);
-        else if (map[i][j] == 'p')
-        {
-          g.setColor(Color.GREEN);
-          spawnX = j*TILE_SIZE;
-          spawnY = i*TILE_SIZE;
-        }
-        else if (map[i][j] == 's')
-          g.setColor(Color.GREEN);
-        else if (map[i][j] == 'n')
-          g.setColor(Color.RED);
-        else if (map[i][j] == 'g')
-          g.setColor(Color.RED);
-        
-        tileX = j*TILE_SIZE;
-        tileY = i*TILE_SIZE;
+    home = true;
+    instructions = false;
+    levels = false;
 
-        g.fillRect(tileX, tileY, 50, 50);
-        g.setColor(Color.DARK_GRAY);
-        g.drawRect(tileX, tileY, 50, 50);
-        //g.drawImage(coin, coinX, coinY, null);
-        g.drawImage(obstacle, obstacleX, obstacleY, null);
-      } 
+    if (home == true){
+      g2d.drawImage(homeBg,0,0, null);
+      
+      g2d.setColor(new Color(31, 21, 1));
+      g2d.drawRect (220,480,300,80);
+      g2d.setColor(new Color(225, 225, 225));
+      //g2d.setColor(new Color(255,175,175));
+      g2d.fillRect(220,480,300,80);
+      
+      g2d.setColor(new Color(31, 21, 1));
+      g2d.drawRect (570,370,160,50);
+      g2d.setColor(new Color(225, 225, 225));
+      g2d.fillRect(570,370,160,50);
+      
+      g2d.setColor(new Color(31, 21, 1));
+      g2d.drawRect (570,451,160,50);
+      g2d.setColor(new Color(225, 225, 225));
+      g2d.fillRect(570,451,160,50);
+      
+      g2d.setColor(new Color(31, 21, 1));
+      g2d.drawRect (570,524,160,50);
+      g2d.setColor(new Color(225, 225, 225));
+      g2d.fillRect(570,524,160,50);
+      
+      g2d.setColor(new Color(31, 21, 1));
+      g2d.drawRect (570,600,160,50);
+      g2d.setColor(new Color(225, 225, 225));
+      g2d.fillRect(570,600,160,50);
+      
+      g2d.setColor(new Color(31, 21, 1));
+      g2d.drawRect (570,675,160,50);
+      g2d.setColor(new Color(225, 225, 225));
+      g2d.fillRect(570,675,160,50);
+      
+      g2d.drawImage(buttonTxt,535,330, null);
+      g2d.drawImage(instructionButtonTxt,-40,300, null);
+      g2d.drawImage(titleTxt,0,25, null);
     }
-    player.setCurrentX((spawnX + player.getX())/50.0);
-    player.setCurrentY((spawnY + player.getY())/50.0);
-    g.drawImage(playerSprite, (int)(player.getCurrentX()*50), (int)(player.getCurrentY()*50), null);
     
-    for (int i = 1; i <= player.getLives(); i++){
-      g.drawImage(heartCanister, 105 - i*15, 5, null);
+    else if (levels == true){
+      for(int i = 0; i<15;i++)  { 
+        for(int j = 0; j<15;j++)  {
+          if (map[i][j] == 'x')
+            g.setColor(Color.DARK_GRAY);
+          else if (map[i][j] == 'z')
+            g.setColor(Color.PINK);
+          else if (map[i][j] == 'b'){
+            g.setColor(Color.CYAN);
+            obstacleX = j* TILE_SIZE;
+            obstacleY = i * TILE_SIZE;
+          }
+          else if (map[i][j] == '0')
+            g.setColor(Color.CYAN);
+          else if (map[i][j] == 'p')
+          {
+            g.setColor(Color.GREEN);
+            spawnX = j*TILE_SIZE;
+            spawnY = i*TILE_SIZE;
+          }
+          else if (map[i][j] == 's')
+            g.setColor(Color.GREEN);
+          else if (map[i][j] == 'n')
+            g.setColor(Color.RED);
+          else if (map[i][j] == 'g')
+            g.setColor(Color.RED);
+        
+          tileX = j*TILE_SIZE;
+          tileY = i*TILE_SIZE;
+
+          g.fillRect(tileX, tileY, 50, 50);
+          g.setColor(Color.DARK_GRAY);
+          g.drawRect(tileX, tileY, 50, 50);
+          //g.drawImage(coin, coinX, coinY, null);
+          g.drawImage(obstacle, obstacleX, obstacleY, null);
+        } 
+      }
+      player.setCurrentX((spawnX + player.getX())/50.0);
+      player.setCurrentY((spawnY + player.getY())/50.0);
+      g.drawImage(playerSprite, (int)(player.getCurrentX()*50), (int)(player.getCurrentY()*50), null);
+    
+      for (int i = 1; i <= player.getLives(); i++){
+        g.drawImage(heartCanister, 105 - i*15, 5, null);
+      }
+      
+    }
     }
   }
-}
